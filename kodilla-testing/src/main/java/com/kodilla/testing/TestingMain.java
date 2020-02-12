52e2e138fafb4186f18417521d118bdeb65beb03
LolaTomA <tomlichoraj@gmail.com>
2020-02-12 19:58:15 +0100

    test 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(5, 3);
+
+        if (sum == 8) {
+            System.out.println("test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int calcsubt = calculator.subtract(5, 3);
+
+        if (calcsubt == 2) {
+            System.out.println("test Ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
--- kodilla-testing/src/main/java/user/SimpleUser.java
+++ kodilla-testing/src/main/java/user/SimpleUser.java
@@ -1,4 +1,13 @@
 package user;
 
 public class SimpleUser {
+    private String username;
+
+    public SimpleUser(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
