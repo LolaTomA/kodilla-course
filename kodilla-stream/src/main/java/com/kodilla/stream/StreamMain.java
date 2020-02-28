60caa074e33b52c0dee9c9ee8f05c6c1cce8880b
LolaTomA <tomlichoraj@gmail.com>
2020-02-28 20:45:42 +0100

    Zadanie 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,11 +1,34 @@
 package com.kodilla.stream;
 
-import lambda.SaySomething;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
-        SaySomething saySomething = new SaySomething();
-        saySomething.say();
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Bolek", poem -> "ABC" + poem + "ABC");
+        poemBeautifier.beautify("Lolek", poem -> toUpperCase(poem));
+        poemBeautifier.beautify("Tola", poem -> "*" + toLowerCase(poem) + "*");
+        poemBeautifier.beautify("Reksio", poem -> "---" + poem + "-" + toLowerCase(poem) + "-" + toUpperCase(poem));
     }
 }
