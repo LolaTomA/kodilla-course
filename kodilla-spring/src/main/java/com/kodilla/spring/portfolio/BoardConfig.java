a63fdd1105f2cfcd92e19c9b7b5dae945f267ba5
LolaTomA <tomlichoraj@gmail.com>
2020-03-28 11:05:10 +0100

    Zadanie 14.3

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,7 +1,5 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -10,7 +8,7 @@ import org.springframework.context.annotation.Scope;
 public class BoardConfig {
 
     @Bean
-    public Board getBoard(){
+    public Board getBoard() {
         return new Board(getTaskList(),getTaskList(), getTaskList());
     }
 
@@ -19,21 +17,4 @@ public class BoardConfig {
     public TaskList getTaskList() {
        return new TaskList();
     }
-
-//    @Bean(name = "taskList2")
-//    @Scope("prototype")
-//    public TaskList getTaskList2(){
-//        task = "in progress";
-//        taskList.addTask();
-//        return new TaskList(taskList.getTasks());
-//    }
-//
-//    @Bean(name = "taskList3")
-//    @Scope("prototype")
-//    public TaskList getTaskList3(){
-//        task = "task done";
-//        taskList.addTask();
-//        return new TaskList(taskList.getTasks());
-//    }
-
 }
