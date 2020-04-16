ad712a5b1f017c5f719423bdcb93ed6a51001dfe
LolaTomA <tomlichoraj@gmail.com>
2020-04-16 20:07:08 +0200

    Zadanie 17.2

--- kodilla_hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla_hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,21 +1,27 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
-public final class TaskList {
+public class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
 
     }
 
-    public TaskList(String listName) {
+    public TaskList(String listName, String description) {
         this.listName = listName;
+        this.description = description;
     }
 
     @Id
@@ -35,6 +41,16 @@ public final class TaskList {
         return description;
     }
 
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -45,5 +61,9 @@ public final class TaskList {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
